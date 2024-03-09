536abdddedd087fd6e74640e1256de2b5e03e9d9
Dương Huy Hoàng <88271496+henryhoangduong@users.noreply.github.com>
2024-03-09 23:22:14 +0700

    Delete backend directory
    Signed-off-by: Dương Huy Hoàng <88271496+henryhoangduong@users.noreply.github.com>

--- backend/src/core/base.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from 'express';
-import { BaseResponse } from './base.response';
-
-export abstract class BaseController {
-  async executeService(res: Response, response: BaseResponse<any>) {
-    return res.status(response.code).json(response);
-  }
-}
