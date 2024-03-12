004f881a4fa088c4c9fd89d0bed59558e74655d2
DuongHuyHoang <henryhoangduong@gmail.com>
2024-03-12 20:04:19 +0700

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [email, setEmail] = useState("");
 
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Home
-                email={email}
-                loggedIn={loggedIn}
-                setLoggedIn={setLoggedIn}
-              />
-            }
-          />
-          <Route
-            path="/login"
-            element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />}
-          />
-        </Routes>
-      </BrowserRouter>
+      <p>hello world</p>
     </div>
   );
 }
