4ce38af86321f0605af8aa8a99cdc423880910b5
henryhoangduong <henryhoangduong@gmail.com>
2024-03-16 10:40:05 +0700

    commit

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -2,8 +2,17 @@ const client = require('../database/supabase.js')
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
 
-async function signin(email, password) {
-    const data =await client.from('student').select('*').eq('')
+async function signin(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const data = await client.from('student').select('*').eq();
+    //Check email
+    if () {
+        
+    }//Check password
+    else {
+        
+    }
 }
 
 function signout() {
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,11 +8,21 @@ function authTeacher(req,res,next){
     }
 }
 
-function authStudent() {
+function authStudent(req,res,next) {
         if (req.body.role==='student') {
         next();
     } else {
             return res.status(401).json(
+            'You are not authorized'
+        )
+    }
+}
+
+function authAdmin(){
+    if (req.body.role === 'admin') {
+        next();
+    } else {
+        return res.status(401).json(
             'You are not authorized'
         )
     }
