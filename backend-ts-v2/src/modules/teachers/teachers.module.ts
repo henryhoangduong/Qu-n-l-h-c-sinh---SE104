8eed9dcaafdd508452f99d8ce6137d77b2ca8090
Vu Huy Hoang <uchihavuhuyhoang@gmail.com>
2024-04-16 20:45:23 +0700

    fix module

--- backend-ts-v2/src/modules/teachers/teachers.module.ts
+++ backend-ts-v2/src/modules/teachers/teachers.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TeachersController } from './teachers.controller';
 import { TeacherService } from './teachers.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Teacher } from '../entitie/entities/Teacher';
+import { Teacher } from '../../entitie/entities/Teacher';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Teacher])],
