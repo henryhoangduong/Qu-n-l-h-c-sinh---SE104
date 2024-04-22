d76bb608bfdc229a4c4f40bfc29dd11249c1c006
henryhoangduong <henryhoangduong@gmail.com>
2024-04-22 11:43:29 +0700

    add module student and create two controllers create and read

---
+++ backend-ts-v2/src/modules/student/student.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Hocsinh } from '../../entities';
+import { StudentCreateDto } from 'src/data-object/student-create.dto';
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Hocsinh)
+    private readonly hocsinhRepository: Repository<Hocsinh>,
+  ) {}
+  async create(studentCreateDto: StudentCreateDto): Promise<Hocsinh> {
+    const student = {
+      ...studentCreateDto,
+      mahocsinh: Math.floor(Math.random() * 100000) + 1,
+    };
+    const newStudent = this.hocsinhRepository.create(student);
+    return this.hocsinhRepository.save(newStudent);
+  }
+  findAll(): Promise<Hocsinh[]> {
+    return this.hocsinhRepository.find();
+  }
+}
