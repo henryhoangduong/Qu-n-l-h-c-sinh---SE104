dba3e4bfade8e15540d29461c4a4876d2b86aae2
henryhoangduong <henryhoangduong@gmail.com>
2024-04-24 16:20:56 +0700

    write add student to class function

--- backend-ts-v2/src/modules/chitietdslop/chitietdslop.service.ts
+++ backend-ts-v2/src/modules/chitietdslop/chitietdslop.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { Chitietdslop } from 'src/entities';
 import { ClassService } from '../class/class.service';
 import { StudentService } from '../student/student.service';
+import { ChitietdslopDto } from 'src/data-object/chitietdslop.dto';
 
 @Injectable()
 export class ChitietdslopService {
@@ -14,14 +15,26 @@ export class ChitietdslopService {
     private studentService: StudentService,
   ) {}
 
-  async create(): Promise<void> {
+  async create(chitietdslopDto: ChitietdslopDto): Promise<void> {
     const data = new Chitietdslop();
-    const lop = await this.classService.findOne(1);
-    const hocsinh = await this.studentService.findOne(1);
-    data.machitietdslop = 1;
-    data.malop = lop;
-    data.mahocsinh = hocsinh;
-    const chitietdslop = this.chitietdslopRepository.create(data); // Create an instance of the entity
-    await this.chitietdslopRepository.save(chitietdslop); // Save the entity
+    const lop = await this.classService.findOne(chitietdslopDto.classid);
+    console.log(chitietdslopDto.studentlists);
+    const chitietdslops = chitietdslopDto.studentlists.map(
+      async (StudentDto) => {
+        const mahocsinh = StudentDto;
+        const hocsinh = await this.studentService.findOne(mahocsinh);
+        const data = new Chitietdslop();
+        data.machitietdslop = Math.floor(Math.random() * 100000) + 1;
+        data.malop = lop;
+        data.mahocsinh = hocsinh;
+
+        const chitietdslop = this.chitietdslopRepository.create(data);
+        await this.chitietdslopRepository.save(chitietdslop);
+        console.log(chitietdslop);
+        return chitietdslop;
+      },
+    );
+
+    await Promise.all(chitietdslops);
   }
 }
