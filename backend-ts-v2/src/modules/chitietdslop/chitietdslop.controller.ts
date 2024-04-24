25f340e0a3a15861bec75887333884f62031df34
henryhoangduong <henryhoangduong@gmail.com>
2024-04-24 16:20:56 +0700

    write add student to class function

--- backend-ts-v2/src/modules/chitietdslop/chitietdslop.controller.ts
+++ backend-ts-v2/src/modules/chitietdslop/chitietdslop.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Body, Post } from '@nestjs/common';
 import { ChitietdslopService } from './chitietdslop.service';
+import { ChitietdslopDto } from 'src/data-object/chitietdslop.dto';
 @Controller('chitietdslop')
 export class ChitietdslopController {
   constructor(private readonly chitietdslopService: ChitietdslopService) {}
-  @Get()
-  create(): Promise<void> {
-    return this.chitietdslopService.create();
+  @Post()
+  create(@Body() chitietdslopDto: ChitietdslopDto): Promise<void> {
+    return this.chitietdslopService.create(chitietdslopDto);
   }
 }
