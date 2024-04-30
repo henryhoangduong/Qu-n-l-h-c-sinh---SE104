9ef808312b994ae3931ea2acd791dcda7fea77b6
henryhoangduong <henryhoangduong@gmail.com>
2024-04-30 13:58:01 +0700

    baoc cao mon va hoc ky controller

--- backend-ts-v2/src/modules/baocao/baocao.controller.ts
+++ backend-ts-v2/src/modules/baocao/baocao.controller.ts
@@ -2,8 +2,34 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('baocao')
 export class BaocaoController {
-  @Get()
-  read(): string {
-    return 'hello world';
+  @Get('mon')
+  mon(): any {
+    const arr = [
+      { lop: '10A1', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '10A2', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '10A3', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '10A4', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '11A1', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '11A2', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '11A3', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '12A1', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '12A2', siso: 40, sld: 20, tile: 0.5 },
+    ];
+    return arr;
+  }
+  @Get('hocki')
+  hocki(): any {
+    const arr = [
+      { lop: '10A1', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '10A2', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '10A3', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '10A4', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '11A1', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '11A2', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '11A3', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '12A1', siso: 40, sld: 20, tile: 0.5 },
+      { lop: '12A2', siso: 40, sld: 20, tile: 0.5 },
+    ];
+    return arr;
   }
 }
