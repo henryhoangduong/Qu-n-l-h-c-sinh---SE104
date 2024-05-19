1b807b42f1c1566a92a609dd112571bf73610666
Vu Huy Hoang <uchihavuhuyhoang@gmail.com>
2024-05-19 20:04:15 +0700

    layout nhap diem

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -7,25 +7,27 @@ import TongKetHocKi from "../pages/Báo cáo/Tổng kết học kì/TongKetHocKi
 import TongKetMon from "../pages/Báo cáo/Tổng kết môn/TongKetMon";
 import TraCuu from "../pages/Tra cứu/TraCuu";
 import XepLop from "../pages/Xếp lớp/XepLop";
+import NhapDiem from "../pages/Nhập điểm/NhapDiem";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/AuthProvider";
 
 const routes = [
-    { path: config.routes.themhocsinh, component:Home , layout: DefaultLayout },
+    { path: config.routes.themhocsinh, component: Home, layout: DefaultLayout },
     { path: config.routes.bangdiem, component: BangDiem, layout: DefaultLayout },
     { path: config.routes.tongkethocki, component: TongKetHocKi, layout: DefaultLayout },
-    {path: config.routes.tongketmon, component: TongKetMon, layout: DefaultLayout},
+    { path: config.routes.tongketmon, component: TongKetMon, layout: DefaultLayout },
     { path: config.routes.change, component: Change, layout: DefaultLayout },
     { path: config.routes.home, component: Home, layout: DefaultLayout },
     { path: config.routes.tracuu, component: TraCuu, layout: DefaultLayout },
+    { path: config.routes.nhapdiem, component: NhapDiem, layout: DefaultLayout },
     { path: config.routes.xeplop, component: XepLop, layout: DefaultLayout },
 ];
 
 const PrivateRoutes = () => {
     const user = useAuth();
-    console.log('user token: ',user.token)
+    console.log('user token: ', user.token)
     if (!user.token) return <Navigate to="/login" />;
     return <Outlet />;
 }
 
-export { routes,PrivateRoutes };
+export { routes, PrivateRoutes };
