6a5aacc7ef383ea9a254bfb8487cfc77378bd912
henryhoangduong <henryhoangduong@gmail.com>
2024-05-27 19:22:44 +0700

    adjust bao cao function

--- backend-ts-v2/src/modules/baocao/baocao.module.ts
+++ backend-ts-v2/src/modules/baocao/baocao.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BaocaoService } from './baocao.service';
 import { BaocaoController } from './baocao.controller';
+import { ClassModule } from '../class/class.module';
 
 @Module({
+  imports: [ClassModule],
   providers: [BaocaoService],
   controllers: [BaocaoController],
 })
